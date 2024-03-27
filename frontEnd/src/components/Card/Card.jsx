37650065eaf592ad543c75a87fdffb3d41b5abfe
F.society <sultanalasadi7@gmail.com>
2024-03-27 12:32:37 +0300

    Add Showcase component and update Ad component and Navbar component

---
+++ frontEnd/src/components/Card/Card.jsx
@@ -0,0 +1,25 @@
+import classes from "./Card.module.css";
+const Card = ({ img, title, price, discount }) => {
+  return (
+    <div
+      className="product"
+      style={{
+        width: "14rem",
+        margin: "0 auto",
+      }}
+    >
+      <div className={classes.imageBox}>
+        <img src={img} alt="productImage" className="img-fluid" />
+      </div>
+      <div className={classes.details}>
+        <h5>{title}</h5>
+        <div className={classes.prices}>
+          <span>{`$${price}`}</span>
+          <span className={classes.discount}> {`$${discount}`}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
