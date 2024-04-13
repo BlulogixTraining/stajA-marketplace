3bc7a7ee4cc634250496fb3c65b402444c6e3845
F.society <sultanalasadi7@gmail.com>
2024-04-13 23:31:29 +0300

    Add express-fileupload package for handling file uploads

--- frontEnd/src/pages/Products.jsx
+++ frontEnd/src/pages/Products.jsx
@@ -1,33 +1,37 @@
-import Showcase from "../components/Showcase/Showcase";
-import shirt from "../assets/shirt.jpg";
 import Yellow from "../assets/Yellow.png";
-import { NavLink, Outlet } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import Filters from "../components/Filters/Filters";
-
+import Card from "../components/Card/Card";
 const Products = () => {
   const products = [
-    { id: 1, name: "Product 1" },
-    { id: 2, name: "Product 2" },
-    { id: 3, name: "Product 3" },
+    { id: 1, name: "Product 1", price: 100, discount: 50 },
+    { id: 2, name: "Product 2", price: 100, discount: 50 },
+    { id: 3, name: "Product 3", price: 100, discount: 50 },
+    { id: 1, name: "Product 1", price: 100, discount: 50 },
+    { id: 2, name: "Product 2", price: 100, discount: 50 },
+    { id: 3, name: "Product 3", price: 100, discount: 50 },
+    { id: 1, name: "Product 1", price: 100, discount: 50 },
+    { id: 2, name: "Product 2", price: 100, discount: 50 },
+    { id: 3, name: "Product 3", price: 100, discount: 50 },
   ];
   return (
     <>
-      <div className="d-block d-lg-flex">
-          <Filters />
-          <div>
-            <h1>Products</h1>
+      <div className="d-block d-lg-flex container ">
+        <Filters />
+        <div className="container">
+          <h2 className="my-4 fw-bold ">Products</h2>
+          <div className="row">
             {products.map((product) => (
-              <div key={product.id}>
-                <NavLink to={`/products/${product.id}`}>{product.name}</NavLink>
+              <div key={product.id} className=" col-md-4 col-sm-6">
+                <Card
+                  title={product.name}
+                  img={Yellow}
+                  price={product.price}
+                  discount={product.discount}
+                />
               </div>
             ))}
-          
-          <div className="d-block d-lg-flex">
-            <Showcase title="" img={shirt} />
-            <Showcase title="" img={Yellow} />
-            <Showcase title="" img={shirt} />
           </div>
-          <Outlet />
         </div>
       </div>
     </>
