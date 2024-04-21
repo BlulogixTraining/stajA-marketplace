568f914f156ab07a1be14c3b81ed838fe596734c
F.society <sultanalasadi7@gmail.com>
2024-04-21 12:32:22 +0300

    Add cors package and update dependencies in package.json and package-lock.json

--- frontEnd/src/components/Showcase/Showcase.jsx
+++ frontEnd/src/components/Showcase/Showcase.jsx
@@ -1,36 +1,16 @@
 import classes from "./Showcase.module.css";
 import Card from "../Card/Card";
 
-const Showcase = ({ title, img }) => {
+const Showcase = ({ title, img, price, discount, headtitle }) => {
   return (
     <div className="container text-center mt-5  ">
-      <h1 className={classes.title}>{title}</h1>
+      <h1 className={classes.title}>{headtitle}</h1>
 
       <div className="row justify-content-center gap-5 gap-lg-3 mt-5">
-        <Card
-          img={img}
-          title="T-SHIRT WITH TAPE DETAILS"
-          price="120"
-          discount="260"
-        />{" "}
-        <Card
-          img={img}
-          title="T-SHIRT WITH TAPE DETAILS"
-          price="120"
-          discount="260"
-        />{" "}
-        <Card
-          img={img}
-          title="T-SHIRT WITH TAPE DETAILS"
-          price="120"
-          discount="260"
-        />
-        <Card
-          img={img}
-          title="T-SHIRT WITH TAPE DETAILS"
-          price="120"
-          discount="260"
-        />
+        <Card img={img} title={title} price={price} discount={discount} />{" "}
+        <Card img={img} title={title} price={price} discount={discount} />{" "}
+        <Card img={img} title={title} price={price} discount={discount} />{" "}
+        <Card img={img} title={title} price={price} discount={discount} />{" "}
       </div>
       <button
         type="button"
--- frontEnd/src/pages/Products.jsx
+++ frontEnd/src/pages/Products.jsx
@@ -1,37 +1,67 @@
 import Yellow from "../assets/Yellow.png";
 import Filters from "../components/Filters/Filters";
 import Card from "../components/Card/Card";
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
 
 const Products = () => {
-  const products = [
-    { id: 1, name: "Product 1", price: 100, discount: 50 },
-    { id: 2, name: "Product 2", price: 100, discount: 50 },
-    { id: 3, name: "Product 3", price: 100, discount: 50 },
-    { id: 1, name: "Product 1", price: 100, discount: 50 },
-    { id: 2, name: "Product 2", price: 100, discount: 50 },
-    { id: 3, name: "Product 3", price: 100, discount: 50 },
-    { id: 1, name: "Product 1", price: 100, discount: 50 },
-    { id: 2, name: "Product 2", price: 100, discount: 50 },
-    { id: 3, name: "Product 3", price: 100, discount: 50 },
-  ];
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get("http://localhost:3000/products");
+        setProducts(response.data.products.slice(0, 3));
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    setIsLoading(false);
+
+    fetchProducts();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="container d-flex  justify-content-center mt-5  ">
+        <div className="spinner-border text-success" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>{" "}
+        <div className="mx-3 fw-bold text-success fs-5"> Loading... </div>
+      </div>
+    );
+  }
+
+  console.log(products);
   return (
     <>
-      <div className="d-block d-lg-flex container ">
+      <div className="d-block d-lg-flex container-fuild ">
         <Filters />
         <div className="container">
+          {isLoading && <div>Loading...</div>}
+
           <h2 className="my-4 fw-bold ">Products</h2>
           <div className="row">
-            {products.map((product) => (
-              <div key={product.id} className=" col-md-4 col-sm-6">
-                <Card
-                  title={product.name}
-                  img={Yellow}
-                  price={product.price}
-                  discount={product.discount}
-                />
-              </div>
-            ))}
+            {products.map(
+              (product) => (
+                console.log(product.image),
+                (
+                  <div key={product.id} className=" col-md-4 ">
+                    <Card
+                      title={product.name}
+                      img={Yellow}
+                      price={product.price}
+                      discount={product.price}
+                    />
+                  </div>
+                )
+              )
+            )}
           </div>
         </div>
       </div>
