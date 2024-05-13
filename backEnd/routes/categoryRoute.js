a27091027bdc69a3c975f03bebfb0b466e165eaa
Bashir Al-Rayes <bashiralrayes6@gmail.com>
2024-05-14 00:41:20 +0300

    edit-2

--- backEnd/routes/categoryRoute.js
+++ backEnd/routes/categoryRoute.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const categoryController = require("../controllers/api/v1/categoryController");
-const VariantCategoryController = require("../controllers/api/v1/VariantCategoryController");
-const verifyToken = require('../middleware/api/v1/verifytoken');
+const verifyToken = require("../middleware/api/v1/verifytoken");
 const router = express.Router();
 
 router.route("/").get(categoryController.getAllCategory);
-router.route("/").post(verifyToken,categoryController.createCategory);
-
-router.route("/variant").get(VariantCategoryController.getAllVariantCategory);
-router.route("/variant").post(verifyToken,VariantCategoryController.createVariantCategory);
+router.route("/").post(verifyToken, categoryController.createCategory);
 
 module.exports = router;
