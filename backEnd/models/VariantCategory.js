aee783cd0575ceaaf765083e14852bd3e2daae39
Bashir Al-Rayes <bashiralrayes6@gmail.com>
2024-05-30 20:30:59 +0300

    Revert "Edit role middleware"
    This reverts commit e25e63dddc8631a0e3e2d383e58799a7fa458354.

--- backEnd/models/VariantCategory.js
+++ backEnd/models/VariantCategory.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const slugify = require("slugify");
 
 const Schema = mongoose.Schema;
 
@@ -11,22 +10,10 @@ const VariantCategorySchema = new Schema({
 
   values: [{ type: String }],
 
-  slug: {
-    type: String,
-    unique: true,
-  },
-
   createdAt: {
     type: Date,
     default: Date.now,
   },
-});
-VariantCategorySchema.pre("validate", function (next) {
-  this.slug = slugify(this.name, {
-    lower: true,
-    strict: true,
-  });
-  next();
 });
 
 const VariantCategory = mongoose.model(
